5c9074d275928aec6bbee213dd35259274499338
Lucas Lima <lucas.alima@souunit.com.br>
2020-08-06 17:45:45 -0300

    fix reponsivity

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -4,16 +4,15 @@ import ladingImg from "../../assets/images/landing.svg";
 import studyIcon from "../../assets/images/icons/study.svg";
 import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
 import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
-import './styles.css';
+import "./styles.css";
 
 export default function Landing() {
   return (
-      <div id="page-lading">
-        <div id="page-lading-content" className="container">
-          <div className="logo-container">
-            <img src={logoImg} alt="Proffy" />
-            <h2>Sua plataforma de estudos online.</h2>
-          </div>
+    <div id="page-lading">
+      <div id="page-lading-content" className="container">
+        <div className="logo-container">
+          <img src={logoImg} alt="Proffy" />
+          <h2>Sua plataforma de estudos online.</h2>
         </div>
 
         <img
@@ -23,21 +22,22 @@ export default function Landing() {
         />
 
         <div className="buttons-container">
-            <a href="./" className="study"> 
-                <img src={studyIcon} alt="Estudar"/>
-                Estudar
-            </a>
+          <a href="./" className="study">
+            <img src={studyIcon} alt="Estudar" />
+            Estudar
+          </a>
 
-            <a href="./" className="give-classes"> 
-                <img src={giveClassesIcon} alt="Dar aulas"/>
-                Dar aulas
-            </a>
+          <a href="./" className="give-classes">
+            <img src={giveClassesIcon} alt="Dar aulas" />
+            Dar aulas
+          </a>
         </div>
 
         <span className="total-connections">
-            Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
+          Total de 200 conexões já realizadas{" "}
+          <img src={purpleHeartIcon} alt="Coração roxo" />
         </span>
       </div>
-
+    </div>
   );
 }
